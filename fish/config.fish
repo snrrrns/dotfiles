2245b758f7ead56314f660e670ff94856edef154
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-12-10 21:54:44 +0900

    Add: fishの設定ファイルを追加

---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# pecoとghqの連携でリポジトリを一覧表示
+function peco_ghq
+    set -l query (commandline)
+    if test -n $query
+        set peco_flags --query "$query"
+    end
+    ghq list --full-path | peco $peco_flags --layout=bottom-up | read recent
+    if [ $recent ]
+        cd $recent
+        commandline -r ''
+        commandline -f repaint
+    end
+end
+
+# pecoとghqのバインディング
+function fish_user_key_bindings
+    bind \cr peco_select_history
+    bind \cg peco_ghq
+end
+
+# エイリアス
+alias dc='docker compose'
